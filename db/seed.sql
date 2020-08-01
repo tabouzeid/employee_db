963092dfdd51dedcc86d1e0d0c392ee963b36172
Taher Abouzeid <tabouzeid@gmail.com>
2020-08-01 18:56:29 -0400

    fixes

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
-use employees;
+use employee_db;
 
 INSERT INTO department
     (name)
--- index.js
+++ index.js
@@ -104,7 +104,7 @@ connection.connect();
 
 getEmployeesQuery = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;";
 getRolesQuery = "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;";
-getDepartmentsQuery = "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;";
+getDepartmentsQuery = "SELECT id, name FROM department;";
 
 addEmployeeQuery = "INSERT INTO employee SET ?";
 addRoleQuery = "INSERT INTO role SET ?";
